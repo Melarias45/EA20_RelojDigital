ff952593407170f877034fec12f0fcfabe8b8979
Melarias45 <22073@portalcedi.edu.mx>
2024-11-15 21:35:29 -0600

    READMEMODIFY AND INDEX

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # EA20_RelojDigital
+
+Melisa Arias Ortega #5
+Ana Nicole Munoz Gonzalez #
+Diego Efrain Gonzalez Marroquin #
+Jorge Alejandro Gonzalez Sanchez #
+Valeria Franco Valle #
---
+++ index.php
@@ -0,0 +1,22 @@
+<html>
+<!DOCTYPE html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>EA20 RelojDigital INDEX</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+
+<div class="py-5 text-center container">
+
+    
+
+</div>
+
+<script src="https://code.jquery.com/jquery-2.2.4.js"></script>
+
+</body>
+
+</html>
